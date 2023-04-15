8359865e42f603f4551c53183710d353a16b3240
Vinit Saah <vsaah@ucsd.edu>
2023-04-15 13:10:05 -0700

    USRP init test code

---
+++ prototype/usrp/build/CMakeFiles/usrp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/usrp.dir/usrp.cpp.o"
+  "usrp"
+  "usrp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/usrp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ prototype/usrp/sample_to_file/build/CMakeFiles/rx_samples_to_file.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rx_samples_to_file.dir/rx_samples_to_file.cpp.o"
+  "rx_samples_to_file"
+  "rx_samples_to_file.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rx_samples_to_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
