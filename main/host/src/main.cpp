20a17484058670dd26e511dc7681ecdb538bf0fc
Vinit Saah <vsaah@ucsd.edu>
2023-06-02 22:02:00 -0700

    Updated the usage of host to make USRP IP optional

--- main/host/src/main.cpp
+++ main/host/src/main.cpp
@@ -12,14 +12,23 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    if (argc != 4) 
+    if (argc < 2) 
     {
-        std::cerr << "Usage: ./host <server_address> <server_port> <usrp_ip>" << std::endl;
+        std::cerr << "Usage: ./host <server_address> <server_port> [usrp_ip]" << std::endl;
         return 1;
     }
-    boost::asio::io_context io_context;
 
-    Client client(io_context, argv[1], argv[2], argv[3]);
+    boost::asio::io_context io_context;
+    std::string usrp_ip;    
+    if (argc == 3)
+    {
+        usrp_ip= "192.168.11.2"; //just a dummy IP
+    }
+    else if (argc == 4)
+    {
+        usrp_ip = argv[3];
+    }
+    Client client(io_context, argv[1], argv[2], usrp_ip);
     client.start();
 
     //client.stop();
